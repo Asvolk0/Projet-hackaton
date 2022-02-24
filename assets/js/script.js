90f9c8a22446dc457144f84b6c9c56aceaf0a8b9
Trinity-prog7 <elena.glav13@gmail.com>
2022-02-24 18:51:39 +0100

    Modification html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,46 @@
 
-
+const errorConfirmPassword = document.querySelector(".error-confirmPassword");
 const form = document.querySelector("form");
 
+password.addEventListener("input", () => {
+  if (
+    password.value.match(
+      /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,24}$/
+    )
+  ) {
+    console.log("True");
+  } else {
+    console.log("False");
+  }
+});
 
-
+confirmPassword.addEventListener("input", () => {
+  if (password.value === confirmPassword.value) {
+    errorConfirmPassword.textContent = "Les mots de passe correspondent";
+    errorConfirmPassword.style.color = "green";
+  } else {
+    errorConfirmPassword.textContent = "Les mots de passe ne correspondent pas";
+    errorConfirmPassword.style.color = "red";
+  }
+});
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-  
-    if (
-    
-      cgt.checked
-    ) {
-      alert("Votre formulaire a été soumis");
-      clearForm();
-    } else {
-      alert("Veuillez tout remplir svp !");
-    }
-  });
-  
-  function clearForm() {
-    cgt.checked = false;
+  e.preventDefault();
+
+  if (
+    password.value === confirmPassword.value &&
+    charte.checked
+  ) {
+    alert("Votre formulaire a été soumis");
+    clearForm();
+  } else {
+    alert("Veuillez tout remplir svp !");
   }
+});
+
+
+
+  
+function clearForm() {
+    cgt.checked = false;
+}
